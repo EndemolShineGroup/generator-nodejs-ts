86bf414f1fda4e7ff29780bc6b78a1bb03d0b23f
Bill Spooner <bill.spooner@endemolshine.com>
2018-10-03 16:30:04 +0100

    chore: remove project generated files and add them to .gitignore

--- generators/app/templates/prettier.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  arrowParens: 'always',
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  parser: 'typescript',
-  semi: true,
-  singleQuote: true,
-  trailingComma: 'all',
-};
--- generators/app/templating.d.ts
+++
@@ -1,9 +0,0 @@
-import Generator from "yeoman-generator";
-export declare type FixedGenerator = Generator & {
-    answers: Generator.Answers;
-};
-export declare const Files: string[];
-export declare const PublicFiles: string[];
-export declare const PrivateFiles: string[];
-declare const _default: (gen: FixedGenerator) => void;
-export default _default;
