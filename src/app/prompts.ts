4e804f48c50fa9a00ed0dea99c823c1a4aaa27e1
Bill Spooner <bill.spooner@endemolshine.com>
2018-10-04 17:38:24 +0100

    chore(typing): improved typing in templates.ts and templates.spec.ts

--- src/app/prompts.ts
+++ src/app/prompts.ts
@@ -1,39 +1,42 @@
+import Generator from 'yeoman-generator';
 
-const filter = (input : string) => {
+const filter = (input: string): string => {
   return input.trim();
 };
 
-export default [
+const prompts: Generator.Questions = [
   {
-    type    : 'input',
-    name    : 'projectScope',
-    message : 'NPM scope: ',
-    default : '@endemolshinegroup',
+    type: 'input',
+    name: 'projectScope',
+    message: 'NPM scope: ',
+    default: '@endemolshinegroup',
     filter,
   },
   {
-    type    : 'input',
-    name    : 'projectName',
-    message : 'Project Name: ',
-    filter
-  },
-  {
-    type    : 'input',
-    name    : 'projectDescription',
-    message : 'Description: ',
-    default : 'sick project...',
+    type: 'input',
+    name: 'projectName',
+    message: 'Project Name: ',
     filter,
   },
   {
-    type    : 'input',
-    name    : 'version',
-    message : 'Version: ',
-    default : '0.0.1',
+    type: 'input',
+    name: 'projectDescription',
+    message: 'Description: ',
+    default: 'sick project...',
     filter,
   },
   {
-    type    : 'confirm',
-    name    : 'isPublic',
-    message : 'Is this a public package?',
-  }
+    type: 'input',
+    name: 'version',
+    message: 'Version: ',
+    default: '0.0.1',
+    filter,
+  },
+  {
+    type: 'confirm',
+    name: 'isPublic',
+    message: 'Is this a public package?',
+  },
 ];
+
+export default prompts;
