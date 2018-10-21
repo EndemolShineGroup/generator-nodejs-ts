b0f75f59d938b9a3f920172d3cb4b7c0843a7bea
Hassan Khan <hassan.khan@endemolshine.com>
2018-10-21 04:50:01 +0100

    test(git): add test case for non-Git initialised projects

--- src/app/index.spec.ts
+++ src/app/index.spec.ts
@@ -80,7 +80,27 @@ describe('EndemolShineGroupGenerator', () => {
     });
 
     it('adds .git folder to project', () => {
-      assert.file('.git');
+      assert.file(path.join(OUTPUT_PATH, PROJECT_NAME, '.git'));
+    });
+  });
+
+  describe('Generates a non-git initialized project correctly', () => {
+    const OUTPUT_PATH = path.join(os.tmpdir(), uuid());
+
+    beforeEach(async () => {
+      return generate(OUTPUT_PATH, {
+        ...answers,
+        isPublic: true,
+        useGit: false,
+      });
+    });
+
+    afterEach(() => {
+      rimraf.sync(OUTPUT_PATH);
+    });
+
+    it('does not generate a .git file when useGit is set to false', () => {
+      assert.noFile(path.join(OUTPUT_PATH, PROJECT_NAME, '.git'));
     });
   });
 
