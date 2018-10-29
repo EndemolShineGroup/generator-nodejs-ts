25ce4f92952bcc242d03119f55a5b6efa7af6fba
Hassan Khan <hassan.khan@endemolshine.com>
2018-10-29 02:07:06 +0000

    fix(scripts): remove invalid subgenerator name

--- scripts/copyTemplates.sh
+++ scripts/copyTemplates.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-for file in app build github node process repo services style typescript; do
+for file in build github node process repo services style typescript; do
   cp -R "src/${file}/templates" "generators/${file}";
 done;
 unset file;
