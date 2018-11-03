fc32f875041ef97b72660162e3fbd2cc59e85a2f
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-03 01:14:29 +0000

    refactor: tidies

--- src/repo/index.ts
+++ src/repo/index.ts
@@ -1,7 +1,7 @@
 import AbstractGenerator from '../lib/AbstractGenerator';
 import files from './files';
 
-export = class NodeGenerator extends AbstractGenerator {
+export = class RepoGenerator extends AbstractGenerator {
   constructor(args: string | string[], options: {}) {
     super(args, options, __dirname);
 
--- src/style/index.ts
+++ src/style/index.ts
@@ -5,20 +5,20 @@ export = class StyleGenerator extends AbstractGenerator {
   constructor(args: string | string[], options: {}) {
     super(args, options, __dirname);
 
-    this.option('projectName', {
-      description: 'Project Name: ',
-      type: String,
-    });
-    this.option('isPublic', {
-      description: 'Is this a public package?',
-      type: Boolean,
-    });
     this.option('addPrettier', {
       description: 'Add Prettier configuration?',
       type: Boolean,
     });
     this.option('addTSLint', {
       description: 'Add TSLint configuration?',
+      type: Boolean,
+    });
+    this.option('projectName', {
+      description: 'Project Name: ',
+      type: String,
+    });
+    this.option('isPublic', {
+      description: 'Is this a public package?',
       type: Boolean,
     });
   }
