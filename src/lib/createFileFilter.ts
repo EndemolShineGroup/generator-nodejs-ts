38a9af35e509cd1de76d7f01e11d956726e60a31
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-03 01:28:39 +0000

    docs(lib): add docblocks to utility functions

--- src/lib/createFileFilter.ts
+++ src/lib/createFileFilter.ts
@@ -1,3 +1,7 @@
+/**
+ * Creates a filter function to use in `Array.filter()`
+ * @param filterString
+ */
 export default (filterString: string) => {
   return (fileName: string) => {
     return !fileName.includes(filterString);
